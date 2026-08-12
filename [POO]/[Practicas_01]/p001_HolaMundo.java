d43f0b363c343f106a4bcd54acce1639c633fbd2
Alangeforce <alangeforce@gmail.com>
2026-08-12 18:27:37 -0500

    Agregar p001_HolaMundo - practica 1 de POO

---
+++ [POO]/[Practicas_01]/p001_HolaMundo.java
@@ -0,0 +1,7 @@
+// Muestra un mensaje de saludo en pantalla
+public class p001_HolaMundo {
+public static void main(String[] args) {
+System.out.print("\033[H\033[2J");
+System.out.println("Hola Mundo, bienvenidos a la programacion en Java");
+}
+}
