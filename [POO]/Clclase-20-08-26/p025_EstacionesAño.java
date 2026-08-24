5038ae52298091fbe62a164b388d756765ecf1fd
Aklan <akllanoficial@gmail.com>
2026-08-24 08:04:51 -0600

    auto: cambios 2026-08-24 08:04:51

--- [POO]/Clclase-20-08-26/p025_EstacionesAño.java
+++ [POO]/Clclase-20-08-26/p025_EstacionesAño.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class p025_EstacionesAño{
     public static void main(String[] args) {
         
        System.out.print("\033[H\033[2J");
        System.out.println(:"Dame un numero entero entre 1 y 4 y te dire que estacion del year es :");
-       int num = new Scanner(System.in),
+       int num = new Scanner(System.in).nextLine();
 
 
        if( num == 1 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
