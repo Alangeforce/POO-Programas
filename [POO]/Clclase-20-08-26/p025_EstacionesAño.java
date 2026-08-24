2f239aa2699fde7db612cbbe2929590df201d0d5
Aklan <akllanoficial@gmail.com>
2026-08-24 07:59:54 -0600

    auto: cambios 2026-08-24 07:59:54

--- [POO]/Clclase-20-08-26/p025_EstacionesAño.java
+++ [POO]/Clclase-20-08-26/p025_EstacionesAño.java
@@ -5,7 +5,7 @@ public class p025_EstacionesAño{
        System.out.println(:"Dame un numero entero entre 1 y 4 y te dire que estacion del year es :");
 
 
-       if num 
+       if( num --)
     }
 
 }
