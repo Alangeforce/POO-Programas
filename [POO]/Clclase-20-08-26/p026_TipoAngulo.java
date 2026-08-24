1b8e5d489565f5ebecba3965fbd1a0ad199bc2be
Aklan <akllanoficial@gmail.com>
2026-08-24 08:19:08 -0600

    auto: cambios 2026-08-24 08:19:08

--- [POO]/Clclase-20-08-26/p026_TipoAngulo.java
+++ [POO]/Clclase-20-08-26/p026_TipoAngulo.java
@@ -2,7 +2,7 @@ public class p026_TipoAngulo {
 
     public static void main(String[] args) {
         
-                System.out.print("\033[H\033[2J"); System.out.flush();
+        System.out.print("\033[H\033[2J"); System.out.flush();
 
         //Entrada
         System.out.println("Dame un numero entero entre 1 y 4 y te dire que estacion del year es :");
