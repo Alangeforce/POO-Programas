a8577b5430e15cea8fdefb912d3b0d49098d9f40
Aklan <akllanoficial@gmail.com>
2026-08-24 08:14:56 -0600

    auto: cambios 2026-08-24 08:14:56

--- [POO]/Clclase-20-08-26/p025_EstacionesAño.java
+++ [POO]/Clclase-20-08-26/p025_EstacionesAño.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
-public class p025_EstacionesAño{
+public class p025_EstacionesAño {
     public static void main(String[] args) {
-        
-       System.out.print("\033[H\033[2J");
-       System.out.println(:"Dame un numero entero entre 1 y 4 y te dire que estacion del year es :");
-       int num = new Scanner(System.in).nextInt();
+        System.out.print("\033[H\033[2J"); System.out.flush();
 
-       if( num >= 1 && num <=4)
-       if( num == 1 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
-       if( num == 2 )System.out.println("\nEs VERANO : JUNIO - JULIO -AGOSTO");
-       if( num == 3 )System.out.println("\nEs OTONO : SEPTIEMBRE - OCTUBRE -NOVIEMVRE");
-       if( num == 4 )System.out.println("\nEs INVIERNO : DICIEMBRE - ENERO -FEBRERO");
+        //Entrada
+        System.out.println("Dame un numero entero entre 1 y 4 y te dire que estacion del year es :");
+        int num = new Scanner(System.in).nextInt();
 
-       
+        //Proceso
 
+        //Salida
+        if (num == 1) {
+            System.out.println("\nEs PRIMAVERA : MARZO - ABRIL - MAYO");
+        } else if (num == 2) {
+            System.out.println("\nEs VERANO : JUNIO - JULIO - AGOSTO");
+        } else if (num == 3) {
+            System.out.println("\nEs OTONO : SEPTIEMBRE - OCTUBRE - NOVIEMBRE");
+        } else if (num == 4) {
+            System.out.println("\nEs INVIERNO : DICIEMBRE - ENERO - FEBRERO");
+        } else {
+            System.out.println("\nNumero invalido, debe ser entre 1 y 4");
+        }
     }
-
 }
