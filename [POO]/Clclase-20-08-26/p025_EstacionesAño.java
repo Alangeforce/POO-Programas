6b01741e4d5aae04f4d723a067814c2fe67d08d0
Aklan <akllanoficial@gmail.com>
2026-08-24 08:07:26 -0600

    auto: cambios 2026-08-24 08:07:26

--- [POO]/Clclase-20-08-26/p025_EstacionesAño.java
+++ [POO]/Clclase-20-08-26/p025_EstacionesAño.java
@@ -9,9 +9,9 @@ public class p025_EstacionesAño{
 
 
        if( num == 1 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
-       if( num ==  )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
-       if( num == 1 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
-       if( num == 1 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
+       if( num == 2 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
+       if( num == 3 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
+       if( num == 4 )System.out.println("\nEs PRIMAVERA : MARZO - ABRIL -MAYO");
 
        
 
