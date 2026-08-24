48cba1a46d16b1f89fd8da3c01699340be910ff5
Aklan <akllanoficial@gmail.com>
2026-08-24 08:38:17 -0600

    auto: cambios 2026-08-24 08:38:17

--- [POO]/Clclase-20-08-26/p027_AceptarEstudiante.java
+++ [POO]/Clclase-20-08-26/p027_AceptarEstudiante.java
@@ -8,7 +8,8 @@ public class p027_AceptarEstudiante {
         System.out.print("\033[H\033[2J"); System.out.flush();
 
         System.out.println("UNIVERSIDAD PATIO DE KSA DE CV");
-        System.out.println("Control de Acceso son mayores de 18 ");
+        System.out.println("Control de Acceso son mayores de 18 y califiaciones mayores a 8\n");
+        
 
 
     }
