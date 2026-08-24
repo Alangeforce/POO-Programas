2c5315c187f858dc463f02b00fe4ea8749e53045
Aklan <akllanoficial@gmail.com>
2026-08-24 08:35:46 -0600

    auto: cambios 2026-08-24 08:35:45

--- [POO]/Clclase-20-08-26/p027_AceptarEstudiante.java
+++ [POO]/Clclase-20-08-26/p027_AceptarEstudiante.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class p027_AceptarEstudiante {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
         
+        Scanner obj = new Scanner(System.in);
+
     }
 }
