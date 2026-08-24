b577f02a6fd163f72263b77698022dcbe0620162
Aklan <akllanoficial@gmail.com>
2026-08-24 08:06:17 -0600

    auto: cambios 2026-08-24 08:06:17

--- [POO]/Clclase-20-08-26/p025_EstacionesAño.java
+++ [POO]/Clclase-20-08-26/p025_EstacionesAño.java
@@ -5,6 +5,7 @@ public class p025_EstacionesAño{
         
        System.out.print("\033[H\033[2J");
        System.out.println(:"Dame un numero entero entre 1 y 4 y te dire que estacion del year es :");
+       S
        int num = new Scanner(System.in).nextInt();
 
 
