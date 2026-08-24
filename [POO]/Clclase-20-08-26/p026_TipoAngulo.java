eb5a087cefc23e27fe1a60aa7c50262b8b541fdc
Aklan <akllanoficial@gmail.com>
2026-08-24 08:25:06 -0600

    auto: cambios 2026-08-24 08:25:06

--- [POO]/Clclase-20-08-26/p026_TipoAngulo.java
+++ [POO]/Clclase-20-08-26/p026_TipoAngulo.java
@@ -21,7 +21,7 @@ public class p026_TipoAngulo {
 
         }
         else //Si el angulo no es correcto manda este mensaje de error s
-            System.out.println();
+            System.out.println("\n");
 
 
 
