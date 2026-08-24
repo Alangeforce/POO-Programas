9ee13204c40428bd6885ee010c4818e1e5ed3797
Aklan <akllanoficial@gmail.com>
2026-08-24 08:19:27 -0600

    auto: cambios 2026-08-24 08:19:27

--- [POO]/Clclase-20-08-26/p026_TipoAngulo.java
+++ [POO]/Clclase-20-08-26/p026_TipoAngulo.java
@@ -5,7 +5,9 @@ public class p026_TipoAngulo {
         System.out.print("\033[H\033[2J"); System.out.flush();
 
         //Entrada
-        System.out.println("Dame un angulo entre 0 y 360 grados :");
+        System.out.println("Dame un angulo entre 0 y 360 grados y te dire que tipo es :");
+
+        
 
 
 
