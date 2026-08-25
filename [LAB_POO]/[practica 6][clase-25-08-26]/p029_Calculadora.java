a1ebf22baa2482a6e9a50935821e06282be1e48d
Aklan <akllanoficial@gmail.com>
2026-08-25 07:52:32 -0600

    auto: cambios 2026-08-25 07:52:32

--- [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
+++ [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
@@ -3,8 +3,9 @@ import java.util.Scanner;
 
 public class p029_Calculadora{
     public static void main(String[] args) {
+        int n1=
         Scanner obj = new Scanner(System.in);
-        
+
 
 
     }
