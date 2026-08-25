bc996f15a6f31f0d02ac35646b884eb5b116ad2c
Aklan <akllanoficial@gmail.com>
2026-08-25 08:04:31 -0600

    auto: cambios 2026-08-25 08:04:31

--- [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
+++ [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
@@ -18,7 +18,7 @@ public class p029_Calculadora{
             case '+';
                 System.out.printf("La suma de %f + %f = %f", n1, n2, n1 + n2); break;
             default;
-                System.out.print
+                sosu
         }
         
 
