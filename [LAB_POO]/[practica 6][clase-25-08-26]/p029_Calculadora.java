c12936b7325a56b00412b28304a3dbab6eb72631
Aklan <akllanoficial@gmail.com>
2026-08-25 07:55:28 -0600

    auto: cambios 2026-08-25 07:55:28

--- [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
+++ [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
@@ -12,6 +12,7 @@ public class p029_Calculadora{
 
         System.out.print("Dame el primer numero: "); n1 =obj.nextInt();
         System.out.print("Dame el segundo numero: "); n2 =obj.nextInt();
+        System.out.print("Dame el segundo numero: "); n2 =obj.nextInt();
         
 
     }
