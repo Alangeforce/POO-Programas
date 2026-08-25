5d50aab130c89285579a52e06bcb1fa2e3ecfa6f
Aklan <akllanoficial@gmail.com>
2026-08-25 08:05:47 -0600

    auto: cambios 2026-08-25 08:05:47

--- [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
+++ [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
@@ -15,8 +15,8 @@ public class p029_Calculadora{
         System.out.print("Que operador: + - * / ^: "); op =obj.next().charAt(0);
 
         switch (op){
-            case '+':
-                System.out.printf("La suma de %f + %f = %f", n1, n2, n1 + n2); break;
+            case '+':System.out.printf("La suma de %f + %f = %f", n1, n2, n1 + n2); break;
+                
             default:
                 break;
             
