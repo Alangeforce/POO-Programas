10f205bd6c06225119d24b5c6e4d83ae13028482
Aklan <akllanoficial@gmail.com>
2026-08-25 07:52:13 -0600

    auto: cambios 2026-08-25 07:52:13

--- [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
+++ [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 
 public class p029_Calculadora{
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System)
+        Scanner obj = new Scanner(System.for (int idx = 0; idx < lst.size(); idx++) {
+            Object elem = lst.get(idx);
+            
+        }
 
 
     }
