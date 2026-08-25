de6912afb75e9c83d475dcf489b1f9d42d90b537
Aklan <akllanoficial@gmail.com>
2026-08-25 07:55:16 -0600

    auto: cambios 2026-08-25 07:55:16

--- [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
+++ [LAB_POO]/[practica 6][clase-25-08-26]/p029_Calculadora.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class p029_Calculadora{
     public static void main(String[] args) {
         int n1=0, n2=0;
+        char op;
         Scanner obj = new Scanner(System.in);
         System.out.print("\u001b[H\u001b[2J");
 
