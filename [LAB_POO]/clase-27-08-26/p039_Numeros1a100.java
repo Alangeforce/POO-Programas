999e71dfc8dba52ca631f63848763cf3057fb71a
Aklan <akllanoficial@gmail.com>
2026-08-27 07:58:30 -0600

    auto: cambios 2026-08-27 07:58:29

--- [LAB_POO]/clase-27-08-26/p039_Numeros1a100.java
+++ [LAB_POO]/clase-27-08-26/p039_Numeros1a100.java
@@ -1,17 +1,5 @@
 
 public class p039_Numeros1a100 {
     
-    public static void main(String[] args) {
-        
-        System.out.print("\033[H\033[2J");
-        int num;
-
-        num = 1;
-
-        while( num <=100 ){
-            System.out.print(num + "");
-            num++;            
-        }
-        System.out.println("\nProceso terminado...");
-    }
+-
 }
