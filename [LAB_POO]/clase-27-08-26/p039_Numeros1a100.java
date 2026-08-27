192a64a5b9ca6556f84515fcc6b6853152889d01
Aklan <akllanoficial@gmail.com>
2026-08-27 07:43:26 -0600

    auto: cambios 2026-08-27 07:43:26

---
+++ [LAB_POO]/clase-27-08-26/p039_Numeros1a100.java
@@ -0,0 +1,10 @@
+package clase-27-08-26;
+
+public class p039_Numeros1a100 {
+    
+    public static void main(String[] args) {
+        
+        System.out.print("\033[H\033[2J"); System.out.flush();
+        int 
+    }
+}
