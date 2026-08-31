4f271fa5455e8130ae0455d7a1dc588ec0ebf549
Aklan <akllanoficial@gmail.com>
2026-08-31 08:30:57 -0600

    auto: cambios 2026-08-31 08:30:57

--- [POO]/clase-31-08-26/p044_ParesAscendente.java
+++ [POO]/clase-31-08-26/p044_ParesAscendente.java
@@ -1,37 +1,23 @@
-
+// Imprime numeros pares ascendente
 import java.util.Scanner;
-
 public class p044_ParesAscendente {
-    
     public static void main(String[] args) {
-        
+        int n,c,s;
+        char resp;
         Scanner obj = new Scanner(System.in);
-
-        int n, c, suma =0;
-        char r;
-        System.out.print("\33[H\033[2J\033[3J");
-
-        System.out.println("Imprimir numeros paners de 2 a 2 hasta n");
-        System.out.print("Hasta donde ? ");n = obj.nextInt();
-        c = 2;
-
-        while (c == n){
-
-            System.out.printf("%d ",c);
-            suma += c;
-            c +=2;
-
-        }
-        System.out.printf("\nLa suma es %d",suma);
-       
-    
-
-        System.out.print("\nDeseas continuar (S/N) ? "); r = Character.toUpperCase(obj.next().charAt(0));
-
-
-
-
-
+        do {
+            System.out.print("\033[H\033[2J");System.out.flush();
+            System.out.println("Imprime números pares ascendente\n");
+            System.out.print("Hasta donde ? "); n = obj.nextInt();
+            s=0;c=2;
+            while( c<=n ) {
+                System.out.printf("%d ",c);
+                s = s + c;
+                c = c+2;
+            }
+            System.out.printf("\nLa suma es %d",s);
+            System.out.print("\nDeseas continuar (S/N) ? "); resp = Character.toUpperCase(obj.next().charAt(0));
+        } while( resp != 'N' );
+        System.out.println("\nGracias por utilizar este programa !");
     }
-
 }
