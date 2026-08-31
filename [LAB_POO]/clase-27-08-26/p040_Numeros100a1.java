d4477f1b649110b2723c8688e35aa375e9c55f19
Aklan <akllanoficial@gmail.com>
2026-08-31 08:29:10 -0600

    auto: cambios 2026-08-31 08:29:09

--- [LAB_POO]/clase-27-08-26/p040_Numeros100a1.java
+++ [LAB_POO]/clase-27-08-26/p040_Numeros100a1.java
@@ -6,7 +6,7 @@ public class p040_Numeros100a1 {
 
         num = 100;
 
-        while( num <=1 ){
+        while( num >=1 ){
             System.out.print(num + "va en s");
             num--;            
         }
---
+++ [POO]/clase-31-08-26/p044_ParesAscendente.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class p044_ParesAscendente {
+    
+    public static void main(String[] args) {
+        
+        Scanner obj = new Scanner(System.in);
+
+        int n, c, suma =0;
+        char r;
+        System.out.print("\33[H\033[2J\033[3J");
+
+        System.out.println("Imprimir numeros paners de 2 a 2 hasta n");
+        System.out.print("Hasta donde ? ");n = obj.nextInt();
+        c = 2;
+
+        while (c == n){
+
+            System.out.printf("%d ",c);
+            suma += c;
+            c +=2;
+
+        }
+        System.out.printf("\nLa suma es %d",suma);
+       
+        System.out.printf("\n\nDeseas COntinuar (S/N)");
+
+        System.out.print("\nDeseas continuar (S/N) ? "); r = Character.toUpperCase(obj.next().charAt(0));
+
+
+
+
+
+    }
+
+}
